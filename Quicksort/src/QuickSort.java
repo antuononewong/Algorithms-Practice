6a1ced7a164a01f34fadc572f2d789818ff94388
antuononewong <48633290+antuononewong@users.noreply.github.com>
2019-08-12 20:05:18 -0700

    Quicksort partition() swap() implemented
    Next: adjust functions for recursion

--- Quicksort/src/QuickSort.java
+++ Quicksort/src/QuickSort.java
@@ -1,5 +1,6 @@
 /* Quicksort is a divide and conquer algorithm that partitions an array around
- * a pivot element. It is done in-place and this implementation isn't stable.
+ * a pivot element. All elements < go into the left partition and elements > go into the right partition.
+ * Repeat until you reach empty array. It is done in-place and this implementation isn't stable.
  * In the average case, it is O(nlogn), so it's efficient. There are different 
  * variations based on the pivot element. In this case, the last element will be 
  * chosen as the pivot.
@@ -12,20 +13,49 @@ public class QuickSort {
 	
 	public QuickSort(int[] numbers) {
 		this.numbers = numbers;
+		
 	}
 	
 	public void partition() {
+		int pivot = numbers[numbers.length - 1];
+		int midpoint = (int) (Math.ceil(numbers.length / 2));
+		int left_index = 0;
+		int right_index = midpoint + 1;
 		
+		for (int i = 0; i < numbers.length - 1; i++) {
+			if (numbers[i] < pivot) {
+				swap(i, left_index);
+				left_index += 1;
+			}
+			else {
+				if (right_index < numbers.length - 1) {
+					swap(i, right_index);
+					right_index += 1;
+				}
+			}	
+		}
+		swap(numbers.length - 1, midpoint - 1);
+	}
+	
+	public void swap(int x, int y) {
+		int temp = numbers[x];
+		numbers[x] = numbers[y];
+		numbers[y] = temp;
 	}
 	
 	public void print() {
 		System.out.println(Arrays.toString(numbers));
 	}
 	
+	public void recurse() {
+		partition();
+	}
+	
 	public static void main(String[] args) {
-		int[] numbers = {5, 8, 2, 3, 1, 7, 6, 10};
+		int[] numbers = {10, 8, 2, 3, 1, 7, 6, 5};
 		
 		QuickSort sort = new QuickSort(numbers);
+		sort.recurse();
 		sort.print();
 	}
 	

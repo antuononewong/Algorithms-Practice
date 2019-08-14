588e1c542182eaf76ae5ce37e88112df42a2f44a
antuononewong <48633290+antuononewong@users.noreply.github.com>
2019-08-14 11:01:34 -0700

    QuickSort
    - bug fixes with partition for last element pivot

--- Quicksort/src/QuickSort.java
+++ Quicksort/src/QuickSort.java
@@ -18,23 +18,20 @@ public class QuickSort {
 	
 	public void partition() {
 		int pivot = numbers[numbers.length - 1];
-		int midpoint = (int) (Math.ceil(numbers.length / 2));
 		int left_index = 0;
-		int right_index = midpoint + 1;
+		int pivot_index = numbers.length - 1;
 		
-		for (int i = 0; i < numbers.length - 1; i++) {
+		for (int i = 0; i < numbers.length - 2; i++) {
 			if (numbers[i] < pivot) {
 				swap(i, left_index);
 				left_index += 1;
 			}
 			else {
-				if (right_index < numbers.length - 1) {
-					swap(i, right_index);
-					right_index += 1;
-				}
+				swap(pivot_index, pivot_index - 1);
+				swap(pivot_index, i);
+				pivot_index = pivot_index - 1;
 			}	
 		}
-		swap(numbers.length - 1, midpoint - 1);
 	}
 	
 	public void swap(int x, int y) {
@@ -52,7 +49,7 @@ public class QuickSort {
 	}
 	
 	public static void main(String[] args) {
-		int[] numbers = {10, 8, 2, 3, 1, 7, 6, 5};
+		int[] numbers = {10, 5, 2, 3, 1, 8, 6, 7};
 		
 		QuickSort sort = new QuickSort(numbers);
 		sort.recurse();

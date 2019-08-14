469e97912f79510fefcb1860a686672b15829f17
antuononewong <48633290+antuononewong@users.noreply.github.com>
2019-08-14 14:19:29 -0700

    QuickSort
    - bug fixes to logic
    - made quicksort recursive
    - added comments for clarity

--- Quicksort/src/QuickSort.java
+++ Quicksort/src/QuickSort.java
@@ -13,47 +13,59 @@ public class QuickSort {
 	
 	public QuickSort(int[] numbers) {
 		this.numbers = numbers;
-		
 	}
 	
-	public void partition() {
-		int pivot = numbers[numbers.length - 1];
-		int left_index = 0;
-		int pivot_index = numbers.length - 1;
+	// Takes a left and right index indicating a subset of original array. Checks
+	// each element in the array then makes appropriate swaps depending on if < or >
+	// the pivot element.
+	public int partition(int left, int right) {
+		int pivot = numbers[right];
+		int pivot_index = left - 1;
 		
-		for (int i = 0; i < numbers.length - 2; i++) {
-			if (numbers[i] < pivot) {
-				swap(i, left_index);
-				left_index += 1;
-			}
-			else {
-				swap(pivot_index, pivot_index - 1);
+		for (int i = left; i < right; i++) {
+			if (numbers[i] <= pivot) {
+				pivot_index += 1;
 				swap(pivot_index, i);
-				pivot_index = pivot_index - 1;
-			}	
+			}
+		}
+		
+		swap(pivot_index + 1, right);
+		
+		return pivot_index + 1;
+	}
+	
+	// Handles recursion of QuickSort to go into the smaller subsets
+	public void recurse(int left, int right) {
+		if (left < right) {
+			int pivot_index = partition(left, right);
+			recurse(left, pivot_index - 1);
+			recurse(pivot_index + 1, right);
 		}
 	}
 	
+	// Simple swap of elements at index x and y
 	public void swap(int x, int y) {
 		int temp = numbers[x];
 		numbers[x] = numbers[y];
 		numbers[y] = temp;
 	}
 	
+	// Prints out whole array
 	public void print() {
 		System.out.println(Arrays.toString(numbers));
 	}
 	
-	public void recurse() {
-		partition();
-	}
-	
+	// Driver function
 	public static void main(String[] args) {
 		int[] numbers = {10, 5, 2, 3, 1, 8, 6, 7};
 		
 		QuickSort sort = new QuickSort(numbers);
-		sort.recurse();
+		sort.recurse(0, numbers.length - 1);
 		sort.print();
 	}
 	
 }
+
+/* Output
+ * [1, 2, 3, 5, 6, 7, 8, 10]
+*/
